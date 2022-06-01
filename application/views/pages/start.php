53a38318a9a68120acd7a2728f827004c93f4f59
Danny Steinbrecher <danny.steinbrecher@outlook.com>
2022-06-01 14:54:26 +0200

    added grafana iframe test

--- application/views/pages/start.php
+++ application/views/pages/start.php
@@ -1,23 +1,4 @@
  <!--Start Page-->
  <div class="Content arduinoFlexContainer">
-     <div class="arduinoFlexItem">
-        <h4>Raspberry Pi</h4>
-         <p>Uptime: 4h</p>
-         <p>Last Scan: 20min ago</p>
-     </div>
-     <div class="arduinoFlexItem">
-         <h4>Raspberry Pi</h4>
-         <p>Uptime: 4h</p>
-         <p>Last Scan: 20min ago</p>
-     </div>
-     <div class="arduinoFlexItem">
-         <h4>Raspberry Pi</h4>
-         <p>Uptime: 4h</p>
-         <p>Last Scan: 20min ago</p>
-     </div>
-     <div class="arduinoFlexItem">
-         <h4>Raspberry Pi</h4>
-         <p>Uptime: 4h</p>
-         <p>Last Scan: 20min ago</p>
-     </div>
+     <iframe src="http://192.168.178.22:3030/d-solo/eXI5tbggz/test?orgId=1&from=1654066376167&to=1654087976167&panelId=2" width="450" height="200" frameborder="0"></iframe>
 </div>
