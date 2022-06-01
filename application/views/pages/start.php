cdcd04d460901b773c30c8993e1a9d8853e5ecf4
Danny Steinbrecher <danny.steinbrecher@outlook.com>
2022-06-01 15:21:17 +0200

    added grafana iframe test

--- application/views/pages/start.php
+++ application/views/pages/start.php
@@ -1,6 +1,6 @@
  <!--Start Page-->
  <div class="Content arduinoFlexContainer">
-     <iframe src="http://192.168.178.22:3030/d-solo/eXI5tbggz/test?orgId=1&from=1654066376167&to=1654087976167&panelId=2" width="450" height="200" frameborder="0"></iframe>
+     <iframe src="http://192.168.178.22:3030"></iframe>
 
 
 
