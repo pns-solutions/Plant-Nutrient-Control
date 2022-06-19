45033055caffe3a532cfc4f8ec817ab334e84fbb
Danny Steinbrecher <danny.steinbrecher@outlook.com>
2022-06-19 14:57:25 +0200

    updated readme and fixed docker install bug

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -8,13 +8,5 @@ sudo sh get-docker.sh
 #adding your user to the docker group
 sudo usermod -a -G docker "$USER"
 
-#TODO: hier muss das Script ggf getrennt werden
 #restart pi:
-#sudo reboot
-
-#install docker compose
-sudo apt-get install docker-compose
-
-# start docker
-sudo systemctl enable docker # Auto-start on boot
-sudo systemctl start docker # Start right now
+sudo reboot
