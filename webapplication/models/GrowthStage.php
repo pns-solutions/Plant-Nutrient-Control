68998a073a425b9887d6163f6d248887450d4c81
niclas.jarowsky@fh-erfurt.de <Deinemutter1>
2022-06-19 15:40:14 +0200

    Pair Programming - Changes by grey

--- webapplication/models/GrowthStage.php
+++ webapplication/models/GrowthStage.php
@@ -4,47 +4,14 @@ namespace PNS;
 
 class GrowthStage extends BaseModelEs
 {
-    protected $growthStageId;
-    protected $name;
-    protected $nutrientArray;
-    protected $createdAt;
-    protected $updatedAt;
-    protected $defaultDuration;
+    const TABLENAME = 'GROWTHSTAGE';
 
-    public function __construct($growthStageId, $name, $nutrientArray, $createdAt, $updatedAt, $defaultDuration) {
-        $this->growthStageId = $growthStageId;
-        $this->name = $name;
-        $this->nutrientArray = $nutrientArray;
-        $this->createdAt = $createdAt;
-        $this->updatedAt = $updatedAt;
-        $this->defaultDuration = $defaultDuration;
-    }
-
-    public function addNutrientToGrowthStage($nutrient){
-        $this->nutrientArray[] = $nutrient;
-    }
-
-    public function growthStageJsonString(){
-        $json1 = '{
-            "growthStageId": "' . $this->growthStageId . '",
-            "name": "' . $this->name . '",
-            "nutrientArray": [';
-
-        $json2 = '';
-        for ($x = 0; $x < count($this->nutrientArray); $x++) {
-            if ($x == count($this->nutrientArray) - 1){
-                $json2 .= $this->nutrientArray[$x]->nutrientJsonString();
-            } else{
-                $json2 .= $this->nutrientArray[$x]->nutrientJsonString() . ",";
-            }
-        }
-        $json2 .= '],';
-        $json3 = '
-            "createdAt": "' . $this->createdAt . '",
-            "updatedAt": "' . $this->updatedAt . '",
-            "defaultDuration": "' . $this->defaultDuration . '"
-        }';
-
-        return $json1 . $json2 . $json3;
-    }
+    protected $schema = [
+        'growthStageId'       => ['type' => BaseModel::TYPE_INT   ],
+        'name'                => ['type' => BaseModel::TYPE_STRING],
+        'nutrientArray'       => ['type' => BaseModel::TYPE_ARRAY ],
+        'createdAt'           => ['type' => BaseModel::TYPE_STRING],
+        'upadtedAt'           => ['type' => BaseModel::TYPE_STRING],
+        'defaultDuration'     => ['type' => BaseModel::TYPE_STRING]
+    ];
 }
