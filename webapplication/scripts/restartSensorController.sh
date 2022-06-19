664f800ed0dcc78fd59ec22b63359f0dac1069c0
Danny Steinbrecher <danny.steinbrecher@outlook.com>
2022-06-19 15:46:54 +0200

    updated sensorcontroller restart script

--- webapplication/scripts/restartSensorController.sh
+++ webapplication/scripts/restartSensorController.sh
@@ -4,4 +4,6 @@ pkill -9 php
 sleep 5
 
 #start application
-php ../controller/sensorcontroller.php
+#php ../controller/sensorcontroller.php
+exec php ../controller/sensorcontroller.php > /dev/null 2>&1 & echo $
+
