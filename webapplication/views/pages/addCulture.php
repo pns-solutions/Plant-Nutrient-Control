17071fc26bc2ce70eabd1b13dadb255f7851a576
Niclas <niclas.jarowsky@fh-erfurt.de>
2022-06-20 14:04:10 +0200

    - started add Culture structure

--- webapplication/views/pages/addCulture.php
+++ webapplication/views/pages/addCulture.php
@@ -4,7 +4,7 @@ $plantId = (isset($_GET['plantId']) && isset($plant)) ? '&plantId=' . $plant['id
 
 <!--FORM-->
 <div class="Content">
-    <form autocomplete="off" action="?c=pages&a=addPlant<?=$plantId?>" method="post" id="formAddPlant">
+    <form autocomplete="off" action="?c=pages&a=addCulture<?=$plantId?>" method="post" id="formAddPlant">
 
         <div>
             <h1 style="color: #ffffff">Pflanze <?=isset($_GET['plantId']) ? 'bearbeiten!' : 'anlegen!'?></h1>
@@ -27,29 +27,40 @@ $plantId = (isset($_GET['plantId']) && isset($plant)) ? '&plantId=' . $plant['id
         <?php if(isset($_GET['plantId'])) : ?>
             <div class="tab">
                 <?php foreach ($plant['growthStages'] as $stage) : ?>
-                    <button type="button" class="tablinks" onclick="openStage(event, 'growthStage<?=$stage['growthStageId']?>//')"><?=$stage['name']?></button>
+                    <button type="button" class="tablinks" onclick="openStage(event, 'growthStage_<?=$stage['name']?>')"><?=$stage['name']?></button>
                 <?php endforeach;?>
                 <button type="button" class="tablinks" onclick="openStage(event, 'addGrowthStage')">Hinzufügen</button>
             </div>
             <?php foreach ($plant['growthStages'] as $stage) : ?>
 
-                <div id="growthStage<?=$stage['id']?>" class="tabcontent">
+<!--            --><?php //var_dump($stage);?>
+                <div id="growthStage_<?=$stage['name']?>" class="tabcontent">
                     <h3><?=$stage['name']?></h3>
                     <table id="nutritionTable">
                         <tr>
                             <th colspan="2" onclick="sortTable(this, 'nutritionTable')">Nährstoff <i class="fas fa-sort"></i></th>
                             <th colspan="2" onclick="sortTable(this, 'nutritionTable')">Menge <i class="fas fa-sort"></i></th>
                         </tr>
+                        <?php $index = 0?>
                         <?php foreach ($stage['nutrientArray'] as $nutrition) : ?>
                             <tr>
-                                <td colspan="2"><?=$nutrition['element']?></td>
-                                <td colspan="2"><?=$nutrition['amount']?></td>
+                                <td colspan="2">
+                                    <select id="<?=$stage['name'] . '_' . $nutrition['name']?>">
+                                        <option value=""></option>
+                                    </select>
+                                    <input type="text" id="<?=$stage['name'] . '_' . $nutrition['name'] . '_nutrientName'?>" name="<?=$stage['name'] . '_' . $nutrition['name'] . '_nutrientName'?>" placeholder="Nährstoffname" value="<?=$nutrition['element']?>">
+                                </td>
+                                <td colspan="2">
+                                    <input type="text" id="<?=$stage['name'] . '_' . $nutrition['name'] . '_nutrientAmount'?>" name="<?=$stage['name'] . '_' . $nutrition['name'] . '_nutrientAmount'?>" placeholder="Menge" value="<?=$nutrition['amount']?>">
+                                </td>
                             </tr>
+                            <?php $index++?>
                         <?php endforeach;?>
+                        <input type="hidden" id="newStageIndex" name="newStageIndex" value="<?=$index?>">
                         <table>
                             <tr>
-                                <td><input type="text" placeholder="Nährstoffname"></td>
-                                <td><input type="text" placeholder="Menge"></td>
+                                <td><input type="text" id="<?=$stage['name']?>_newNutrient" name="<?=$stage['name']?>_newNutrient" placeholder="Nährstoffname"></td>
+                                <td><input type="text" id="<?=$stage['name']?>_newAmount" name="<?=$stage['name']?>_newAmount" placeholder="Menge"></td>
                                 <td>
                                     <button type="submit"><i class="fa-solid fa-floppy-disk"></i></button>
                                 </td>
@@ -58,6 +69,29 @@ $plantId = (isset($_GET['plantId']) && isset($plant)) ? '&plantId=' . $plant['id
                     </table>
                 </div>
             <?php endforeach;?>
+            <div id="addGrowthStage" class="tabcontent">
+                <div class="input">
+                    <label for="newGrowthStageName">Name der Wachstumsstufe</label>
+                    <input type="text" id="newGrowthStageName" name="newGrowthStageName" placeholder="Name der Wachstumsstufe">
+                    <span class="error-message" id="errorNewGrowthStageName"></span>
+                </div>
+                <table id="nutritionTable">
+                    <tr>
+                        <th colspan="2" onclick="sortTable(this, 'nutritionTable')">Nährstoff <i class="fas fa-sort"></i></th>
+                        <th colspan="2" onclick="sortTable(this, 'nutritionTable')">Menge <i class="fas fa-sort"></i></th>
+                    </tr>
+
+                </table>
+                <table>
+                    <tr>
+                        <td><input type="text" placeholder="Nährstoffname"></td>
+                        <td><input type="text" placeholder="Menge"></td>
+                        <td>
+                            <button type="submit"><i class="fa-solid fa-floppy-disk"></i></button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
         <?php endif;?>
 
         <!-- buttons -->
