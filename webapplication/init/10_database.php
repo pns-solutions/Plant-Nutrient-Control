806a57b34f15b24f7d79c62dbc6da0e7874e86fd
Danny Steinbrecher <danny.steinbrecher@outlook.com>
2022-06-24 00:24:40 +0200

    added timestamp to main index added server ip as global variable

--- webapplication/init/10_database.php
+++ webapplication/init/10_database.php
@@ -6,12 +6,12 @@ use Elasticsearch\Common\Exceptions\Missing404Exception;
 const INDEX = 'pns';
 
 $elasticsearchConnection = null;
+$server = '192.168.XXX.XXX';
 
 try {
 //    error_to_logFile('verbunden');
-    $elasticsearchConnection = ClientBuilder::create()->setHosts(['51.75.64.177'])->build();
+    $elasticsearchConnection = ClientBuilder::create()->setHosts([$server])->build();
 } catch (Missing404Exception $e) {
 //    error_to_logFile('Verbindung nicht möglich!');
     die('Verbindung nicht möglich!');
 }
-
