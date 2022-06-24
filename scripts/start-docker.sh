c9bf4e8fd58570582b4011ce42162b65d232f2a1
Danny Steinbrecher <danny.steinbrecher@outlook.com>
2022-06-24 17:27:37 +0200

    updated Readme

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-#install docker compose
-sudo apt-get install docker-compose
-
 # start docker
 sudo systemctl enable docker # Auto-start on boot
 sudo systemctl start docker # Start right now
