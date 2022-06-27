fe93aad7f7999b38766656c6da1d40d23df022c7
niclas.jarowsky@fh-erfurt.de <Deinemutter1>
2022-06-27 18:08:47 +0200

    - fixed erros in basemodel

--- webapplication/views/shared/navMenuBar.php
+++ webapplication/views/shared/navMenuBar.php
@@ -7,16 +7,13 @@
     </div>
     <div class="menuLinksFlexContainer">
         <a href="<?=$_SERVER['SCRIPT_NAME']?>/?c=pages&a=start" class="menuLinksFlexItem">
-            <h4 <?=($_GET['a'] == 'start') ? 'class="navSelected"' : ''?>>Arduinoüberwachung</h4>
+            <h4 <?=($_GET['a'] == 'start') ? 'class="navSelected"' : ''?>>Grafana</h4>
         </a>
         <a href="<?=$_SERVER['SCRIPT_NAME']?>/?c=pages&a=CultureManagement" class="menuLinksFlexItem">
             <h4 <?=($_GET['a'] == 'plantManagement') ? 'class="navSelected"' : ''?>>Pflanzenverwaltung</h4>
         </a>
         <a href="<?=$_SERVER['SCRIPT_NAME']?>/?c=pages&a=fertilizerManagement" class="menuLinksFlexItem">
             <h4 <?=($_GET['a'] == 'fertilizerManagement') ? 'class="navSelected"' : ''?>>Düngerverwaltung</h4>
-        </a>
-        <a href="https://play.grafana.org/d/000000012/grafana-play-home?orgId=1" target="_blank" class="menuLinksFlexItem">
-            <h4>Grafana</h4>
         </a>
     </div>
 </div>
