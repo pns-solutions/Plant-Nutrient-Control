5ff643842ce1dd11f5b0055ed37dbeba01d28a24
niclas.jarowsky@fh-erfurt.de <Deinemutter1>
2022-06-27 19:19:44 +0200

    - fixed error

--- webapplication/views/shared/navMenuBar.php
+++ webapplication/views/shared/navMenuBar.php
@@ -1,7 +1,6 @@
 <div class="NavContent">
     <div class="logoNav">
         <a href="<?=$_SERVER['SCRIPT_NAME']?>/?c=pages&a=start">
-            <!--        <img src="--><?//=PAGE_IMAGE_PATH.'/blocknroll/NUR_Kopf_Rot.png'?><!--">-->
             <h2>PNS Solution</h2>
         </a>
     </div>
@@ -9,7 +8,7 @@
         <a href="<?=$_SERVER['SCRIPT_NAME']?>/?c=pages&a=start" class="menuLinksFlexItem">
             <h4 <?=($_GET['a'] == 'start') ? 'class="navSelected"' : ''?>>Grafana</h4>
         </a>
-        <a href="<?=$_SERVER['SCRIPT_NAME']?>/?c=pages&a=CultureManagement" class="menuLinksFlexItem">
+        <a href="<?=$_SERVER['SCRIPT_NAME']?>/?c=pages&a=cultureManagement" class="menuLinksFlexItem">
             <h4 <?=($_GET['a'] == 'plantManagement') ? 'class="navSelected"' : ''?>>Pflanzenverwaltung</h4>
         </a>
         <a href="<?=$_SERVER['SCRIPT_NAME']?>/?c=pages&a=fertilizerManagement" class="menuLinksFlexItem">
