23a5e4f18f524ecb84a36329ec35f5439a2a7593
Danny Steinbrecher <danny.steinbrecher@outlook.com>
2022-06-28 21:28:31 +0200

    updated cronjobs files

--- scripts/addCronTab.sh
+++ scripts/addCronTab.sh
@@ -3,7 +3,7 @@
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "* * * * * docker exec webapplication /restart.sh" >> mycron
+echo "* * * * * docker exec webapplication /scipts/restart.sh" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
