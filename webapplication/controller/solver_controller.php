b75d12d88dc7ca6a393906199637311595e21d2d
niclas.jarowsky@fh-erfurt.de <Deinemutter1>
2022-06-30 14:00:29 +0200

    - server ip now from globals array - added try catch for MQTT - added error catch for validation

--- webapplication/controller/solver_controller.php
+++ webapplication/controller/solver_controller.php
@@ -14,9 +14,22 @@ $port = 8883;
 $clientId = rand(5, 15);
 
 $connectionSettings = new ConnectionSettings();
-$mqtt = new MqttClient($server, $port, $clientId);
+$server = $GLOBALS['server'];
 
-$mqtt->connect($connectionSettings, false);
+//echo $server;
+try {
+    $mqtt = new MqttClient($server, $port, $clientId);
+} catch (ProtocolNotSupportedException $e) {
+    printf("MqttClientException: There was an Error while setting up the MqttClient\n Exceptions was: %s\n", $e);
+}
+
+try {
+    $mqtt->connect($connectionSettings, false);
+} catch (ConfigurationInvalidException $e) {
+    printf("MqttClientException: There was an Error while connect to the MqttClient\n Exceptions was: %s\n", $e);
+} catch (ConnectingToBrokerFailedException $e) {
+    printf("MqttClientException: There was an Error while connect to the MqttClient\n Exceptions was: %s\n", $e);
+}
 
 $message = \PNS\SolverResult::solveNutrientSolution();
 echo $message;
