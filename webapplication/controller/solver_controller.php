9484dd76be8e112e763c9db5b5dc0578b5c0ee79
niclas.jarowsky@fh-erfurt.de <Deinemutter1>
2022-06-30 14:00:24 +0200

    - server ip now from globals array - added try catch for MQTT - added error catch for validation

--- webapplication/controller/solver_controller.php
+++ webapplication/controller/solver_controller.php
@@ -1,18 +1,17 @@
 <?php
 
-require('../assets/composer/vendor/autoload.php');
-require('../core/functions.php');
-require('../init/10_database.php');
+require_once(__DIR__ . '/../assets/composer/vendor/autoload.php');
+require_once(__DIR__ . '/../core/functions.php');
+require_once(__DIR__ . '/../init/10_database.php');
 
+use PhpMqtt\Client\Exceptions\ConfigurationInvalidException;
+use PhpMqtt\Client\Exceptions\ConnectingToBrokerFailedException;
+use PhpMqtt\Client\Exceptions\ProtocolNotSupportedException;
 use \PhpMqtt\Client\MqttClient;
 use \PhpMqtt\Client\ConnectionSettings;
 
-
-$server = '192.168.0.102';
-//$server = 'localhost';
 $port = 8883;
 $clientId = rand(5, 15);
-//$clientId = 'test-publisher';
 
 $connectionSettings = new ConnectionSettings();
 $mqtt = new MqttClient($server, $port, $clientId);
@@ -20,4 +19,5 @@ $mqtt = new MqttClient($server, $port, $clientId);
 $mqtt->connect($connectionSettings, false);
 
 $message = \PNS\SolverResult::solveNutrientSolution();
+echo $message;
 $mqtt->publish('solverResult', $message);
