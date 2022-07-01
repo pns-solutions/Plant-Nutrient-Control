582976a37a9b54c9a9269d675af4babeab0de9a7
Danny Steinbrecher <danny.steinbrecher@outlook.com>
2022-07-01 14:41:07 +0200

    removed old restart script

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,5 +2,4 @@
 docker-compose build && docker-compose up -d
 docker exec -it webapplication sh -c "mkdir -p logs && chmod 755 logs"
 docker exec -it webapplication sh -c "cd assets/composer && composer update &&  composer install"
-#docker exec -d webapplication sh -c "cd scripts && chmod 755 restartControllers.sh && ./restartControllers.sh"
 docker exec -d  webapplication sh -c 'cd scripts && php checkProcess.php'
