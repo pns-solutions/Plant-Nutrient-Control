c4e7a3db880980df85f99119d559e6bc53e3e045
Niclas Jarowsky <56829182+TotalFlash@users.noreply.github.com>
2022-07-02 18:33:34 +0200

    Update start.php
    changed static IP to dynamic global IP

--- webapplication/views/pages/start.php
+++ webapplication/views/pages/start.php
@@ -1,4 +1,4 @@
  <!--Start Page-->
  <div class="Content" style="padding: 0!important;">
-     <iframe style="height: 98%; width: 100%" src="http://192.168.2.188:3030"></iframe>
+     <iframe style="height: 98%; width: 100%" src="<?=$GLOBALS['server'] ?? ''?>"></iframe>
 </div>
