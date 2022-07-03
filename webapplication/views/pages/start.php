082257acf770266561558e0471711bc73c3603db
Niclas Jarowsky <56829182+TotalFlash@users.noreply.github.com>
2022-07-03 13:20:08 +0200

    Update start.php

--- webapplication/views/pages/start.php
+++ webapplication/views/pages/start.php
@@ -1,4 +1,4 @@
  <!--Start Page-->
  <div class="Content" style="padding: 0!important;">
-     <iframe style="height: 98%; width: 100%" src="<?=isset($GLOBALS['server']) ? $GLOBALS['server'] . ':3030' : ''?>"></iframe>
+     <iframe style="height: 98%; width: 100%" src="<?=isset($GLOBALS['server']) ? 'http://' . $GLOBALS['server'] . ':3030' : ''?>"></iframe>
 </div>
