9f52ca12cddf21ef558b2ec9943f84cb5c71b521
dannysteinbrecher <danny.steinbrecher@outlook.com>
2024-02-22 21:57:29 +0100

    elasticsearch durch influxdb ersetzt als Schnittstelle zwischen MQTT Broker und Influx DB wird Telegraf eingesetzt

--- scripts/addCronTab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#write out current crontab
-crontab -l > mycron
-#echo new cron into cron file
-echo "* * * * * docker exec -d  webapplication sh -c 'cd scripts && php checkProcess.php'" > mycron
-#install new cron file
-crontab mycron
-rm mycron
--- webapplication/controller/solver_controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once(__DIR__ . '/../assets/composer/vendor/autoload.php');
-require_once(__DIR__ . '/../core/functions.php');
-require_once(__DIR__ . '/../init/10_database.php');
-
-\PNS\SolverResult::runSolver();
